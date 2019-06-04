be774d3459f314e059ceaade03f07523eeb30d0b
Harish Hardy <mj.harishardy@gmail.com>
2019-06-04 10:12:23 +0530

    Initial Commit

---
+++ jsonParserUrl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  jsonParserUrl
+//
+//  Created by harishardy on 04/06/19.
+//  Copyright © 2019 iosdev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
