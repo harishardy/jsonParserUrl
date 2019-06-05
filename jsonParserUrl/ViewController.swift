53d25f7b35b718926d6805b1e3974c686c0e69fe
Harish Hardy <mj.harishardy@gmail.com>
2019-06-05 11:17:25 +0530

    first

--- jsonParserUrl/ViewController.swift
+++ jsonParserUrl/ViewController.swift
@@ -1,20 +1,79 @@
 //
-//  ViewController.swift
-//  jsonParserUrl
+//  ThirdViewController.swift
+//  JSONUrlParserWithTabBar
 //
-//  Created by harishardy on 04/06/19.
-//  Copyright © 2019 iosdev. All rights reserved.
+//  Created by Greeens5 on 04/06/19.
+//  Copyright © 2019 Book. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
 
+    var linkkey = "https://www.myprivatedeal.com/API/getDeals.php?key=jcjn79b8f043f4y74yh48ug984u"
+    var name = [String] ()
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return name.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellid1", for: indexPath)
+        cell.textLabel?.text = name[indexPath.row]
+        return cell
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        let url = URL(string: linkkey)
+        URLSession.shared.dataTask(with: url!) {(data,response, error) in
+            if error != nil
+            {
+                print(error!)
+            }else{
+                
+                do{
+                    if let json = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.allowFragments) as? [String: Any]
+                    {
+                        
+                        let myarray = json["Deal"] as! NSArray
+                        for arr in myarray
+                        {
+                            if let path = arr as?[String: Any]
+                            {
+                                
+                                self.name.append(path["title"] as! String)
+                                self.name.append(path["distance"] as! String)
+                                self.name.append(path["id_deal"] as! String)
+                                self.name.append(path["deal_type"] as! String)
+                            }
+                        }
+                        DispatchQueue.main.async {
+                            self.tableView.reloadData()
+                        }
+                    }
+                }catch let error as NSError{
+                    print(error)
+                }
+            }
+            }.resume()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
-
